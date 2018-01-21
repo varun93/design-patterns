d7ca61650a653b00aed7cc95bdaefb77ebbc611a
Varun <1hvarun@gmail.com>
2018-01-21 12:38:34 +0530

    formatted the code

--- pubsub.js
+++ pubsub.js
@@ -1,45 +1,44 @@
 class PubSub {
-
-constructor(){
-this.topics = {};
-}
-  
-subscribe = (topic,subscriber) => {
-  const subscribers = this.topics[topic] ? this.topics[topic].filter(s => s !== subscriber) : [];
-	this.topics[topic] = subscribers.concat(subscriber); 
-	return {
-  	unsubscribe : () => {
-      this.topics[topic] = this.topics[topic].filter(s => s!== subscriber);
-     }
+  constructor() {
+    this.topics = {};
   }
+
+  subscribe = (topic, subscriber) => {
+    const subscribers = this.topics[topic]
+      ? this.topics[topic].filter(s => s !== subscriber)
+      : [];
+    this.topics[topic] = subscribers.concat(subscriber);
+    return {
+      unsubscribe: () => {
+        this.topics[topic] = this.topics[topic].filter(s => s !== subscriber);
+      }
+    };
+  };
+
+  publish = (topic, message) => {
+    const subscribers = this.topics[topic] || [];
+    subscribers.forEach(subscriber => subscriber(message));
+  };
 }
 
-publish = (topic,message) => {
-   const subscribers = this.topics[topic] || [];
-	 subscribers.forEach(subscriber => subscriber(message));
-}
-
-}
-
-// driver code 
+// driver code
 
 const pubSub = new PubSub();
 
-const sub1 = (message) => {
-	console.log(`Sub 1 doing somethig with with ${message}`)
-}
+const sub1 = message => {
+  console.log(`Sub 1 doing somethig with with ${message}`);
+};
 
-const sub2 = (message) => {
-	console.log(`Sub 2 doing somethig with with ${message}`)
-}
+const sub2 = message => {
+  console.log(`Sub 2 doing somethig with with ${message}`);
+};
 
-const subscriptionOne  =  pubSub.subscribe('topic1',sub1);
-const subscriptionsTwo  =  pubSub.subscribe('topic1',sub2);
+const subscriptionOne = pubSub.subscribe("topic1", sub1);
+const subscriptionsTwo = pubSub.subscribe("topic1", sub2);
 
 // publish something to topic1 now
-pubSub.publish('topic1',"Tomorrow Surprise test");
+pubSub.publish("topic1", "Tomorrow Surprise test");
 
 subscriptionOne.unsubscribe();
 
-pubSub.publish('topic1',"Tomorrow Surprise test after unsubscribing");
-
+pubSub.publish("topic1", "Tomorrow Surprise test after unsubscribing");
